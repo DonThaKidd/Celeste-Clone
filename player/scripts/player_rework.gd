56a77204964d674ec1d158b017c859f8dd8aeb96
DonThaKidd <liameevee2008@gmail.com>
2025-01-08 17:59:47 +0000

    reimplemented dash
    Added dashing back into the project after it had a bug previously.
    
    - attempted to make a lives system, did not work well.
    - attempted to fix the dashing infinitely bug.

--- player/scripts/player_rework.gd
+++ player/scripts/player_rework.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 ## Movement settings
-const speed = 300.0            # Maximum speed
-const acceleration = 700    # Acceleration rate
-const deceleration = 800     # Deceleration rate
+@export var speed = 300.0            # Maximum speed
+@export var acceleration = 1100    # Acceleration rate
+@export var deceleration = 1200     # Deceleration rate
+
+const DASH_SPEED = 900.0
+var dashing = false
+var can_dash = true
 
 @onready var gc := $GrappleController
 
@@ -11,9 +15,9 @@ const deceleration = 800     # Deceleration rate
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 ## jump time graph
-var jump_height : float = 200
-var jump_time_to_peak : float = 0.4
-var jump_time_to_descent : float = 0.2
+@export var jump_height : float = 200
+@export var jump_time_to_peak : float = 0.4
+@export var jump_time_to_descent : float = 0.2
 
 ## math found on GDC.
 var jump_vel : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
@@ -23,6 +27,7 @@ var fall_grav : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_t
 func _physics_process(delta):
 	velocity.y += get_grav() * delta
 	
+	 
 	var input_direction = Vector2.ZERO
 	
 	
@@ -31,13 +36,32 @@ func _physics_process(delta):
 		input_direction.x += 1
 	if Input.is_action_pressed("left"):
 		input_direction.x -= 1
+	if Input.is_action_pressed("down"):
+		input_direction.y += 1
+	if Input.is_action_pressed("up"):
+		input_direction.y -= 1
 	
 	
-	input_direction = input_direction.normalized()  # Normalize diagonal movement
+	input_direction = round(input_direction)  # Normalize diagonal movement
+	print(input_direction)
+	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		can_dash = false
+		$Dash/dash_timer.start()
+		$Dash/dash_cooldown.start()
 
 	if input_direction != Vector2.ZERO:
-		## Accelerate towards the desired direction
-		velocity = velocity.move_toward(input_direction * speed,acceleration * delta)
+		if dashing:
+			if input_direction.x != 0:
+				velocity.x = input_direction.x * DASH_SPEED
+			elif input_direction.y != 0:
+				velocity.y = input_direction.y * DASH_SPEED
+			elif input_direction.x == 1 and input_direction.y == 1:
+				velocity.y = input_direction.y * DASH_SPEED
+				velocity.x = input_direction.x * DASH_SPEED
+		else:                           ## Accelerate towards the desired direction
+			velocity = velocity.move_toward(input_direction * speed, acceleration * delta)
 	
 	else:
 		## Decelerate to stop
@@ -59,3 +83,10 @@ func get_grav() -> float:
 ## jumping function
 func jump() -> void:
 	velocity.y = jump_vel
+
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
